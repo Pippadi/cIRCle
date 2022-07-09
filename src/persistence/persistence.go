9498900cbf517ae376ea5a623dc6aa2961512e4e
Prithvi Vishak <prithvivishak@gmail.com>
2022-07-09 15:12:08 +0530

    Remove debug prints

--- src/persistence/persistence.go
+++ src/persistence/persistence.go
@@ -22,7 +22,6 @@ func New(a fyne.App) *Persistor {
 func (p *Persistor) LoadConnConfig() *connection.Config {
 	c := &connection.Config{Port: 6667}
 	if cr, err := storage.CanRead(p.uri); cr {
-		log.Println(cr)
 		checkError(err)
 		reader, err := storage.Reader(p.uri)
 		checkError(err)
@@ -36,13 +35,12 @@ func (p *Persistor) LoadConnConfig() *connection.Config {
 
 func (p *Persistor) DumpConnConfig(conf *connection.Config) {
 	if cw, err := storage.CanWrite(p.uri); cw {
-		log.Println(cw)
 		checkError(err)
 		bytes, err := json.MarshalIndent(conf, "", "\t")
 		checkError(err)
 		writer, err := storage.Writer(p.uri)
 		checkError(err)
-		log.Println(writer.Write(bytes))
+		writer.Write(bytes)
 		writer.Close()
 	}
 }
