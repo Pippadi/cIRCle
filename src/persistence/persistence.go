54b48d71d7ef555948d600cebd25a02bb9bc83e9
Prithvi Vishak <prithvivishak@gmail.com>
2022-07-10 15:02:50 +0530

    Make persistor future-proof

--- src/persistence/persistence.go
+++ src/persistence/persistence.go
@@ -21,22 +21,28 @@ func New(a fyne.App) *Persistor {
 
 func (p *Persistor) LoadConnConfig() *connection.Config {
 	c := &connection.Config{Port: 6667}
+	configs := make([]connection.Config, 0) // To allow for multiple connections in the future
 	if cr, err := storage.CanRead(p.uri); cr {
 		checkError(err)
 		reader, err := storage.Reader(p.uri)
 		checkError(err)
 		bytes, err := ioutil.ReadAll(reader)
 		checkError(err)
-		json.Unmarshal(bytes, c)
+		json.Unmarshal(bytes, &configs)
 		reader.Close()
+	}
+	if len(configs) > 0 {
+		c = &(configs[0])
 	}
 	return c
 }
 
 func (p *Persistor) DumpConnConfig(conf *connection.Config) {
+	configs := make([]connection.Config, 1)
+	configs[0] = *conf
 	if cw, err := storage.CanWrite(p.uri); cw {
 		checkError(err)
-		bytes, err := json.MarshalIndent(conf, "", "\t")
+		bytes, err := json.MarshalIndent(&configs, "", "\t")
 		checkError(err)
 		writer, err := storage.Writer(p.uri)
 		checkError(err)
